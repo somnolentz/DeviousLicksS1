23522c703fecb7ccc7bba50272117b9e47a544c9
leen <leeneyadstudy@gmail.com>
2021-11-13 23:36:22 +0200

    Update2

---
+++ Assets/scripts/Countdowntimer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdowntimer : MonoBehaviour
+{
+   public float currentTime = 0f;
+    public float startingTime = 30f;
+    public float timeLeft;
+    public Text countdownText;
+    void Start()
+    {
+       currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <=0)
+        {
+
+            currentTime = 0;
+           
+        }
+       
+
+    }
+   
+
+
+}
