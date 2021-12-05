381b2d70cf26c8233f6c41ecaeba2a4a1a3e425a
leen <leeneyadstudy@gmail.com>
2021-12-05 08:25:21 +0200

    adjusted gamemanager

--- Assets/scripts/gamemanager.cs
+++ Assets/scripts/gamemanager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class gamemanager : MonoBehaviour
 {
     public GameObject gameOverCanvas;
+    public GameObject portalOpenCanvas;
     private void Start()
     {
         Time.timeScale = 1;
