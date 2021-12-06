ca8e31c282b3ae9582faef4e4c65a35eb383d6bd
zubair <z.davids@student.saedubai.com>
2021-12-07 03:52:57 +0400

    Stun and speed boost ability. Stun function not 100%

--- Assets/scripts/PickUp.cs
+++ Assets/scripts/PickUp.cs
@@ -6,10 +6,22 @@ using UnityEngine.UI;
 public class PickUp : MonoBehaviour
 {
     private bool triggered = false;
+
+    public int abilityType; //0 = none , 1 = speedBoost , 2 = ??
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag.Equals("Player") && !triggered)
         {
+            if (abilityType == 1)
+            {
+                collider.GetComponent<speedBoostAbility>().Activate();
+                
+            }
+            else if (abilityType == 2)
+            {
+
+                collider.GetComponent<stunAbility>().Activate();
+            }
 
             FindObjectOfType<ScoringSystem>().Increase();
             GetComponent<AudioSource>().Play();
---
+++ Assets/scripts/speedBoostAbility.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedBoostAbility : MonoBehaviour
+{
+    public float boostingTime = 3;
+    public float boostingSpeed = 10;
+
+    public float boostTimer = 0;
+
+    public bool isBoosting = false;
+    private float intialSpeed;
+    private Thrust player;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponentInParent<Rigidbody>();
+        player = GetComponentInParent<Thrust>();
+    }
+    private void FixedUpdate()
+    {
+        if (isBoosting)
+        {
+            boostTimer += Time.deltaTime;
+            if (boostTimer >= boostingTime)
+            {
+                player.dash = intialSpeed;
+                boostTimer = 0;
+                isBoosting = false;
+
+            }
+        }
+    }
+    public void Activate()
+    {
+        intialSpeed = player.dash;
+        player.dash = boostingSpeed;
+        isBoosting = true;
+    }
+}
